a91626885f67e4dd8f75a2d69dc63ec0f4a268b2
DevMiguelAcevedo <acebedo2524@gmail.com>
2025-07-30 14:54:08 -0500

    Modificacion axios

--- src/api/axios.js
+++ src/api/axios.js
@@ -2,8 +2,12 @@ import axios from "axios";
 
 // Utilizamos la URL base definida en el archivo .env
 const instance = axios.create({
-  baseURL: `${import.meta.env.VITE_API_URL}`,  // VITE_API_URL se define en el archivo .env
-  withCredentials: true,  // Esto permite que las cookies se envíen en las solicitudes
+  baseURL: `${import.meta.env.VITE_API_URL}`, // VITE_API_URL se define en el archivo .env
+  withCredentials: true, // Esto permite que las cookies se envíen en las solicitudes
+  headers: {
+    "Content-Type": "application/json",
+    Accept: "application/json",
+  },
 });
 
 export default instance;
